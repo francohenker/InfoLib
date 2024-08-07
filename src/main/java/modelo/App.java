57874a87499a318ebf33d88c063cdd12b56ffeac
Franco Henker <francohenker@gmail.com>
2024-08-07 16:56:13 -0300

    actualizaciones de login

--- src/main/java/modelo/App.java
+++ src/main/java/modelo/App.java
@@ -27,7 +27,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/vista/prestamoAlta.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/vista/login2.fxml"));
         scene = new Scene(fxmlLoader.load(), 1080, 720);
         stage.setScene(scene);
         stage.show();
